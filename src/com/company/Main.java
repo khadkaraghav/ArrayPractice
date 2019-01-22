300e5e4cb1cf98f8384d526f1aeaaa65e530e1f8
khadkaraghav <khadkaraghav@gmail.com>
2019-01-22 16:06:38 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,122 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+      /*  Scanner input = new Scanner(System.in);
+
+        int num[] = new int[10];
+
+        int sum = 0;
+
+        System.out.println("Enter the inputs");
+
+        for (int i = 0; i < num.length; i++) {
+
+            num[i] = input.nextInt();
+            sum = sum + num[i];
+        }
+
+        System.out.println("The sum of the numbers is"   +sum);
+
+    }
+} */
+
+
+      /*Scanner input = new Scanner(System.in);
+
+      int num[] = new int[10];
+
+      double sum=0, avg = 0;
+
+        System.out.println("Enter the numbers");
+
+        for (int i=0; i<num.length;i++)
+            {
+                num[i] = input.nextInt();
+                sum = sum + num[i];
+                avg = sum/10;
+            }
+        System.out.println("The average of the numbers is"  +avg);
+            }
+    }
+    */
+
+      /* Scanner input = new Scanner(System.in);
+
+        String[] name = new String[3];
+
+        System.out.println("Enter the names");
+
+        for (int i = 0; i < name.length; i++) {
+
+
+            name[i] = input.nextLine();
+
+
+                System.out.println("The names are" + " " + name[i]);
+            }
+
+        }
+    } */
+
+
+ /*       int num[] = new int[]{2, 5, 9, 0, 2, 1, 8, 5, 4};
+
+
+        for (int i = 0; i <= num.length; i++) {
+
+            if (num[i] == 5) {
+
+
+
+                System.out.println(i);
+            }
+        }
+    }
+} */
+
+
+     /*   int num[] = new int[10];
+
+        for (int i =num.length; i>0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}*/
+
+
+     /*   String str[] = new String[]{"w", "t", "y", "h", "k"};
+
+
+        for (int i = 0; i < str.length; i++) {
+
+            if (str[i] == "t") {
+                str[i] = "hello";
+
+            }
+            System.out.println(str[i]);
+        }
+    }
+}*/
+
+
+       /* int firstArray[] = new int[]{1, 7, 6, 5, 9};
+        int secondArray[] = new int[]{2, 7, 6, 3, 4};
+
+
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] == secondArray[i])
+            {
+                System.out.println(firstArray[i]+","+secondArray[i]);
+
+            }
+        }
+    }
+}
+*/
